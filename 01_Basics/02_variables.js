bc0edee211ec89120f5b6d5791b38e2e16213bad
Rishi-Sahu27 <sahurishi1427@gmail.com>
2026-07-06 09:01:53 +0000

    Datatypes

---
+++ 01_Basics/02_variables.js
@@ -0,0 +1,28 @@
+"use strict";   // treat all JS code as newer version
+
+// alert( 3+3 ) // we are using node js , not browser
+
+console.log( 3+3)  // code readibility should be high
+
+console.log (" Rishi ")
+
+let name= "Hitesh" // string datatype
+let age = 18
+let isLoggedIn =  false
+ 
+// number=> 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqueness
+
+
+// object
+
+console.log(typeof " Rishi ");       // Rishi
+console.log(typeof null);          // object
+console.log(typeof undefined );     // undefined
+
+

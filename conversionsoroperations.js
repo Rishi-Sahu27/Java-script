3a833feb9ece4363fe97fcf990c638f2e2cbc9f8
Rishi-Sahu27 <sahurishi1427@gmail.com>
2026-07-06 09:27:52 +0000

    Conversions of Datatype

---
+++ conversionsoroperations.js
@@ -0,0 +1,38 @@
+let score = "33abc"
+// let score = null
+// let score = undefined
+// let bool = true
+// let score = "Rishi"
+ //  let score = "33"      // string
+
+console.log(typeof score);
+console.log(typeof(score) );
+
+let valueInNumber = Number(score)     // now guarranted that no. is a score
+console.log(typeof valueInNumber );
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN; 
+// false => 0 true => 0
+
+
+let IsLoggedIn = "Rishi"
+
+let booleanIsloggedIn = Boolean(IsLoggedIn)
+ 
+console.log(booleanIsloggedIn);
+
+
+// 1=> true ; 0=> false
+// "" => false
+// "Rishi" => true
+
+
+let someNumber = 33
+ let stringNumber = String(someNumber)
+
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+ 

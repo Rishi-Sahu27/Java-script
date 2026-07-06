7e033bb2c2814f61269f80ec896561158c56ea1d
Rishi-Sahu27 <sahurishi1427@gmail.com>
2026-07-06 10:01:24 +0000

    Conversions 2

--- conversionsoroperations.js
+++ conversionsoroperations.js
@@ -36,3 +36,42 @@ let someNumber = 33
  console.log(stringNumber);
  console.log(typeof stringNumber);
  
+
+ // ****************   OPERATIONS ****************//
+
+let value=3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Rishi"
+
+let str3= str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+

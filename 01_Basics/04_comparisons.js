6f82ec1a94195f398a9704e6e9b4393e0f3d4d08
Rishi-Sahu27 <sahurishi1427@gmail.com>
2026-07-08 12:52:18 +0000

    Comparisions

---
+++ 01_Basics/04_comparisons.js
@@ -0,0 +1,35 @@
+console.log(2>1);
+console.log(2 >= 1);
+console.log( 2 < 1);
+console.log(2 == 1);
+console.log( 2!= 1);
+
+
+
+console.log("2">1);
+console.log("02">1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null>= 0 );
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0)
+
+// Avoid doing comparisons like these
+
+// ===   // Strictly checks without conversions into other datatype like == does
+
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+
+
